7f2a84cc1b8367e9c25c4b1a8931d5b52c8dbbd4
mikejemmanuel2 <mikejemmanuel@gmail.com>
2025-10-07 00:31:18 -0700

    Syntax Fix, Automatic "New" System, Update 10/7/25
    Fixed Epic Updater Syntax Issue that caused platform to not be set.
    New system that makes games set as new for 3 updates until being removed from the new section.

--- tools/makeFullLibrary.py
+++ tools/makeFullLibrary.py
@@ -14,10 +14,22 @@ for library in libraryList:
     fullData = fullData | library
 
 with open(dataFolder.joinpath("fullLibrary.json"), "r", encoding="utf-8") as oldJson:
-    oldJsonKeys = json.load(oldJson).keys()
+    oldJsonData = json.load(oldJson)
+    oldJsonKeys = oldJsonData.keys()
     for key in fullData.keys():
         if key not in oldJsonKeys:
             fullData[key]["new"] = True
+            fullData[key]["newStatus"] = 1
+        else:
+            if "newStatus" in oldJsonData[key].keys():
+                fullData[key]["new"] = True
+                fullData[key]["newStatus"] = oldJsonData[key]["newStatus"] + 1
+                if fullData[key]["newStatus"] > 3:
+                    fullData[key].pop("newStatus")
+                    fullData[key].pop("new")
+            elif "new" in oldJsonData[key].keys():
+                fullData[key]["new"] = True
+                fullData[key]["newStatus"] = 1
 
 with open(dataFolder.joinpath("fullLibrary.json"), "w", encoding="utf-8") as newJson:
     json.dump(fullData, newJson, ensure_ascii=False, indent=2)
